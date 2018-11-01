3a9757a3df35d1b9ece2d77f875e786326b17886
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2018-11-01 17:10:50 +0000

    Gallery (#3)
    * Add links container margin
    * Update testimonials section
    * Add gallery images
    * Update profile photos
    * Add gallery placeholder, gallery css, update bios
    * Add gallery

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+
 (function($) {
 
   // Add smooth scrolling to all links in navbar
@@ -31,5 +63,9 @@
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   });
+
+  $('.gallery').carousel({
+    interval: 2000
+  })
 
 })(jQuery);
